9905c887d78de844b70bb1d0bb7570fcae1fe923
jhlee <jhlee@sorin.co.kr>
2022-11-07 16:23:32 +0900

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MainMap/>
+    <SideBar class="side-bar"/>
+  </div>
+</template>
+
+<script>
+import MainMap from '@/components/MainMap'
+import SideBar from '@/components/SideBar'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar,
+    MainMap
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+#app {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  > .side-bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  }
+}
+</style>
